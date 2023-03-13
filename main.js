ba1d248a5acd3809dbc6f702b8c417337347e200
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-03-13 19:04:06 -0400

    styling finished and functionality complete

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+window.addEventListener('load', () => {
+    generatePassword()
+})
 const passwordElement = document.querySelector("#password");
-const passwordLength = document.querySelector("#password-length");
+const passwordLength = document.querySelector("#password-slider");
 const uppercase = document.querySelector("#uppercase");
 const lowercase = document.querySelector("#lowercase");
 const numbers = document.querySelector("#numbers");
 const symbols = document.querySelector("#symbols");
+const checkBoxes = document.querySelectorAll('input[type="checkbox"]')
+const resetButton = document.querySelector('#reset')
+
 
 const lowerChars = ["a", "b", "c", "d", "e", "f", "g",
     "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
@@ -14,25 +20,46 @@ const allSymbols = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", 
 const allDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 passwordLength.addEventListener('mouseup', () => {
-    const password = generateRandomPassword(passwordLength.value, uppercase.checked, lowercase.checked, numbers.checked, symbols.checked)
-    updateText(password);
+    generatePassword()
 })
 
-function updateText(text) {
-    passwordElement.value = text;
+resetButton.addEventListener('click', () => {
+    generatePassword()
+})
+
+
+
+checkBoxes.forEach(checkBox => {
+    checkBox.addEventListener('input', () => {
+        generatePassword();
+    })
+})
+
+function generatePassword() {
+    const password = generateRandomString(passwordLength.value, uppercase.checked, lowercase.checked, numbers.checked, symbols.checked)
+
+    passwordElement.value = password;
 }
 
-function generateRandomPassword(length, uppercase, lowercase, numbers, symbols) {
+
+
+function generateRandomString(length, uppercase, lowercase, numbers, symbols) {
     const availableCharacters = stringBuilder(uppercase, lowercase, numbers, symbols)
     let password = ""
-    for (let i = 0; i < length; i++) {
-        password += availableCharacters[getRandomInt(availableCharacters.length)]
+    if (!uppercase && !lowercase && !numbers && !symbols) {
+        return null
+    } else {
+        for (let i = 0; i < length; i++) {
+            password += availableCharacters[getRandomInt(availableCharacters.length)]
+        }
     }
+
     return password;
 }
 
 function stringBuilder(uppercase, lowercase, numbers, symbols) {
     let availableCharacters = [];
+
     if (uppercase) {
         let upperChars = [].concat(...(lowerChars.map(char => char.toUpperCase().split(''))));
         availableCharacters.push(...upperChars);
